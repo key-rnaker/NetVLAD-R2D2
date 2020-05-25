7eedc79a324452800665f85bd5bd661b398c7253
key-rnamker <jhyeup@naver.com>
2020-05-25 18:01:21 +0900

    NetVLAD model add

--- model_NVLAD.py
+++ model_NVLAD.py
@@ -8,17 +8,16 @@ import torchvision.models as models
 
 # based on https://github.com/Nanne/pytorch-NetVlad/netvlad.py
 class NetVLAD(nn.Module) :
-    def __init__(self) :
+    def __init__(self, clsts, traindescs) :
+        super(NetVLAD, self).__init__()
         self.num_clusters = 64
         self.alpha = 0
-        self.dim = 128
+        self.dim = 512
         self.normalize_input = True
         self.vladv2 = False
-
-        super(NetVLAD, self).__init__()
         self.conv = nn.Conv2d(self.dim, self.num_clusters, kernel_size=(1,1), bias=self.vladv2)
-        self.centroids = nn.Parameter(torch.rand(self.num_clusters, self.dim))
 
+        ############ Convolution Layer ############
         # using pretrained vgg16
         vgg16 = models.vgg16(pretrained=True)
         # remove last relu and maxpool
@@ -29,4 +28,61 @@ class NetVLAD(nn.Module) :
                 parameter.requires_grad = False
 
         self.vgg16 = nn.Sequential(*vgg16_layers)
+
+        #############################################
+
+        ############ NetVLAD layer ############
+        knn = NearestNeighbors(n_jobs=-1)
+        knn.fit(traindescs)
+        del traindescs
+        dsSq = np.square(knn.kneighbors(clsts, 2)[0])
+        del knn
+        self.alpha = (-np.log(0.01) / np.mean(dsSq[:,1] - dsSq[:,0])).item()
+        self.centroids = nn.Parameter(torch.from_numpy(clsts))
+        del clsts, dsSq
+        self.conv.weight = nn.Parameter( (2.0 * self.alpha * self.centroids).unsquuze(-1).unsquuze(-1))
+        self.conv.bias = nn.Parameter( - self.alpha * self.centroids.norm(dim=1))
+
+    def forward(self, image) :
+
+        # 이미지의 local descriptor 추출
+        # x.shape = [batch갯수, encoder_dim=512, H, W]
+        x = vgg16(image)
         
+        # num_batchs = 입력 이미지 갯수
+        # num_channels = local descriptor의 channel 수 == vgg16의 dim == 512
+        num_batchs, num_channels = x.shape[:2]
+
+        if self.normalize_input :
+            x = F.normalize_input(x, p=2, dim=1)
+
+        # soft assignment 계산
+        # local descriptor x_i 한개당 cluster centre 64개의 soft assignment가 필요
+        # soft_assign.shape = [batch갯수, num_clusters=64, H x W]
+        soft_assign = self.conv(x).view(num_batchs, self.num_clusters, -1)
+        soft_assign = F.softmax(soft_assign, dim=1)
+
+        # local descriptor x flatten
+        # x_flatten.shape = [batch갯수,H x W, encoder_dim = 512]
+        # x_flatten = x.view(num_batchs, num_channels, -1)
+        x_flatten = x.view(num_batchs, -1, num_channels)
+
+        # vlad.shape = [batch갯수, num_clusters=64, num_channels=512]
+        vlad = torch.zeros([num_batchs, self.num_clusters, num_channels], dtype=x.dtype, layout=x.layout, device=x.device)
+        
+        # residual.shape = [batch갯수, H x W, encoder_dim = 512]
+        residual = torch.zeros_like(x_flatten)
+
+        for batch in range(num_batchs) :
+            for cluster in range(self.num_clusters) :
+                for i in range(x_flatten.shape[1]) : 
+                    vlad[batch,cluster,:] += soft_assign[batch,cluster,i] * ( x_flatten[batch,i,:] - self.centroids[cluster,:])
+
+        vlad = F.normalize(vlad, p=2, dim=2) # intra-normalization
+        vlad = vlad.view(num_batchs, -1)     # flatten
+        vlad = F.normalize(vlad, p=2, dim=1) # L2-normalization
+
+        # vlad.shape = [num_batchs, num_clusters x num_channels]
+
+        return vlad
+                
